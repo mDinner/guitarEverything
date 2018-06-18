e1e18bbe1ddabe17fe7c97f4d5277a2bb9a21d73
Michael Dinner <mdinner9@gmail.com>
2018-06-18 12:58:13 -0400

    mlab database

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 var monSess    = require('mongoose-session');
 var session    = require('express-session');
+var secrets    = require('./secrets')
 const MongoStore = require('connect-mongo')(session);
 
 var app        = express(); 				// define our app using express
 
+
+console.log('secrets: ', secrets)
 
 // app config
 app.use(bodyParser());
@@ -15,7 +18,11 @@ app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, './app/views'));
 app.use(express.static(__dirname + '/app/public'));
 
-const connection = mongoose.createConnection('mongodb://localhost/test');
+var connectString = 'mongodb://' + secrets.dbUser + ':' + secrets.dbPw + secrets.secrets
+var connectStr = 'mongodb://mdinner:duckSauc3@ds261450.mlab.com:61450/guitar'
+console.log('connectString: ', connectString)
+
+const connection = mongoose.createConnection(secrets.connectStr);
 
 // set up sessions
 app.use(session({
@@ -42,7 +49,7 @@ app.use('/', routes);
 app.use('/user', user);
 app.use('/fx', fx);
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(secrets.connectStr);
 
 var port = process.env.PORT || 8000;
 app.listen(port);
